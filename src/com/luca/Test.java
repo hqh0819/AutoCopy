7412869a0fbcc86221fe3ebd70abdf1cfd7ba76d
qiheng.hu <qiheng.hu@HC0176004.cminl.oa>
2016-08-21 10:15:44 +0800

    aaa

--- src/com/luca/Test.java
+++ src/com/luca/Test.java
@@ -3,6 +3,8 @@
  */
 package com.luca;
 
+import java.util.Calendar;
+
 /**
  * @author qiheng.hu
  * @date 2016年8月19日
@@ -14,11 +16,7 @@ public class Test {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String line="0125 fd\\da\\fd\\fda\\a.txt";
-		System.out.println("D:\\DATA\\import\\lcd\\CELL\\4600\\T36A2\\T36A20E0\\"+line.split(" ")[1].substring(line
-				.split(" ")[1]
-				.lastIndexOf("\\")));
-		System.out.println();
+	System.out.println(new String("abcd.txtx").toUpperCase().endsWith(".TXT"));
 	}
 
 }
